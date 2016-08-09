b7be75fb16af7aa54e3f33f92c5a77c92767d36e
mhmorshe <mahir.morshed@gmail.com>
2016-08-09 23:41:24 +0000

    asdfadsf

--- deploy.php
+++ deploy.php
@@ -1,14 +1,13 @@
 <?php
 // Set Variables
 $LOCAL_ROOT         = "/var/www/html";
-$LOCAL_REPO_NAME    = ".git";
+$LOCAL_REPO_NAME    = "CSE135";
 $LOCAL_REPO         = "{$LOCAL_ROOT}/{$LOCAL_REPO_NAME}";
 $REMOTE_REPO        = "http://github.com/mhmorshe/CSE135.git";
 $BRANCH             = "master";
 
 if ( $_POST['payload'] ) {
   // Only respond to POST requests from Github
-
   if( file_exists($LOCAL_REPO) ) {
 
     // If there is already a repo, just run a git pull to grab the latest changes
