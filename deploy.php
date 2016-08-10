6ec92a2699ef0693f435493f409df64e4929a43a
mhmorshe <mahir.morshed@gmail.com>
2016-08-10 01:14:30 +0000

    asdfadsf

--- deploy.php
+++ deploy.php
@@ -1,37 +1,4 @@
 <?php
-<<<<<<< HEAD
-// Set Variables
-$LOCAL_ROOT         = "/var/www/";
-$LOCAL_REPO_NAME    = "/var/www/html";
-$LOCAL_REPO         = "{$LOCAL_ROOT}/{$LOCAL_REPO_NAME}";
-$REMOTE_REPO        = "git@github.com:mhmorshe/CSE135.git";
-$BRANCH             = "master";
-
-  if($_POST['payload'])
-  {
-    shell_exec("cd /var/www/html");
-    shell_exec("git pull");
-    die("done " . mktime());
-  }
-/*
-if ( $_POST['payload'] ) {
-  // Only respond to POST requests from Github
-  if( file_exists($LOCAL_REPO) ) {
-
-    // If there is already a repo, just run a git pull to grab the latest changes
-    shell_exec("cd {$LOCAL_REPO} && git pull");
-
-    die("done " . mktime());
-  } else {
-
-    // If the repo does not exist, then clone it into the parent directory
-    shell_exec("cd {$LOCAL_ROOT} && git clone {$REMOTE_REPO}");
-
-    die("done " . mktime());
-  }
-}
-*/
-=======
 	/**
 	 * GIT DEPLOYMENT SCRIPT
 	 *
@@ -59,7 +26,6 @@ if ( $_POST['payload'] ) {
 		$output .= htmlentities(trim($tmp)) . "\n";
 	}
 	// Make it pretty for manual user access (and why not?)
->>>>>>> 5bd35c61e2175eec1676d7bd8df3eabbb9b5819d
 ?>
 <!DOCTYPE HTML>
 <html lang="en-US">
